61e149687cd0544232a6fb11b28e3a00df9b9fb6
Cameron Moy <mail@camoy.name>
2023-01-20 17:18:42 -0500

    progress

--- effect/racket.rkt
+++ effect/racket.rkt
@@ -9,57 +9,81 @@
 ;; require
 
 (require (for-syntax racket/base
+                     racket/match
                      racket/syntax
-                     syntax/parse)
+                     syntax/parse
+                     syntax/transformer)
          racket/control
+         racket/list
          racket/match)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; effect sugar
 
-(define effect-pt
+(define effect-prompt-tag
   (make-continuation-prompt-tag))
 
-(struct effect-s ())
+(struct effect (default)
+  #:property prop:procedure
+  (λ (self . args)
+    (define (body kont)
+      (abort/cc effect-prompt-tag (list* kont self args)))
+    (call/comp/check body effect-prompt-tag self args)))
 
-(define-syntax effect?
-  (make-rename-transformer #'effect-s?))
-
-(define-syntax (effect stx)
+(define-syntax (define-effect stx)
   (syntax-parse stx
-    [(_ ?name:id (?field:id ...))
-     #'(struct ?name effect-s (?field ...))]))
+    [(_ (?name:id ?param:id ...) ?body:expr ...)
+     #:with ?arity #`#,(length (syntax->list #'(?param ...)))
+     #:do [(define effect-stx #'(effect (λ (?param ...) ?body ...)))]
+     #:with ?effect-id (syntax-local-lift-expression effect-stx)
+     #'(define-match-expander ?name
+         (make-match-transformer #'?effect-id ?arity)
+         (make-variable-like-transformer #'?effect-id))]))
 
-(define (perform e)
-  (call/comp (λ (kont) (abort/cc effect-pt e kont)) effect-pt))
+(begin-for-syntax
+  (define ((make-match-transformer effect-id arity) stx)
+    (syntax-parse stx
+      [(_ ?p ...)
+       #:fail-unless (= (length (syntax->list #'(?p ...))) arity)
+       "arity mismatch"
+       #`(list (== #,effect-id) ?p ...)])))
 
 (define current-continue (make-parameter #f))
 
+;; TODO: can we guarantee that ?p is an effect pattern?
 (define-syntax (handle stx)
   (syntax-parse stx
     [(_ ?e:expr [?p:expr ?v:expr ...] ...)
      #'(let ()
-         (define (user-handler eff)
-           (match eff
+         (define (user-handler args)
+           (match args
              [?p ?v ...] ...
-             [_ (fallback eff user-handler)]))
+             [_ (fallback args user-handler)]))
          (install-handler (λ () ?e) user-handler))]))
 
 (define (install-handler proc user-handler)
-  (define (handler e kont)
+  (define (handler vs)
+    (match-define (cons kont args) vs)
     (parameterize ([current-continue (wrap kont user-handler)])
-      (user-handler e)))
-  (call/prompt proc effect-pt handler))
+      (user-handler args)))
+  (call/prompt proc effect-prompt-tag handler))
 
 (define (wrap proc user-handler)
   (λ args
     (install-handler (λ () (apply proc args)) user-handler)))
 
-(define (fallback eff user-handler)
-  (call/comp
+(define (fallback args user-handler)
+  (match-define (cons eff more-args) args)
+  (call/comp/check
    (λ (kont)
-     (abort/cc effect-pt eff (extend kont (current-continue))))
-   effect-pt))
+     (define kont* (extend kont (current-continue)))
+     (abort/cc effect-prompt-tag (cons kont* args)))
+   effect-prompt-tag eff more-args))
+
+(define (call/comp/check proc pt eff args)
+  (if (continuation-prompt-available? pt)
+      (call/comp proc pt)
+      (apply (effect-default eff) args)))
 
 (define (extend k1 k2)
   (λ args (call-in-continuation k1 (λ () (apply k2 args)))))
@@ -76,20 +100,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; tests
 
-;; TODO: define-effect with function call-like syntax (is default handler)
-;; effects are not first-class objects
-;; no perform: (print-str ...) will perform (like normal function call)
-;; handle does static arity checking
-
 (module+ test
-  (effect print-str (str))
-  (effect print-num (n))
+  (define-effect (print-str str)
+    (error 'print-str))
+
+  (define-effect (print-num n)
+    (error 'print-num))
+
   (handle
    (handle (let ()
-             (perform (print-str "hello"))
-             (perform (print-num 42))
-             (perform (print-str "world"))
-             (perform (print-num 43)))
+             (print-str "hello")
+             (print-num 42)
+             (print-str "world")
+             (print-num 43))
            [(print-str str)
             (displayln str)
             (continue (void))])
